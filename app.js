3c72ebd68e0b4c43d75f42c08628ff653bba12d3
Diego S Mageski <diego.mageski@gmail.com>
2025-01-29 01:47:56 -0300

    primeira task feita: criada a lista e a funcao add user

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let nomes = [];
+
+function adicionarAmigo(){
+    let nome = document.querySelector('input').value;
+    if (nome!=''){
+        nomes.push(nome);
+    }
+    document.querySelector('input').value='';
+    console.log(nomes.length);
+}
+
+//o length, retorna a quantidade de elementos, se nao tiver nada, retorna 0.
